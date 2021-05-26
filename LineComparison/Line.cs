1691c86246062a4b55be309924f3a9cb5d28c79f
Mayur Wankhade <mayur.wankhade2@gmail.com>
2021-05-26 23:15:48 +0530

    [Mayur] Add : Distance between two points calculated

---
+++ LineComparison/Line.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LineComparison
+{
+    public class Line
+    {
+        public void LengthOfLine(double x1,double y1,double x2, double y2)
+        {
+            double distanceBetwnTwoPoints = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            Console.WriteLine(distanceBetwnTwoPoints);
+        }
+    }
+}
--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -4,9 +4,11 @@ namespace LineComparison
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Console.WriteLine("Welcome to Line Comparison Computation Program");
+            Line line = new Line();
+            line.LengthOfLine(5, 7, 10, 12);
         }
     }
 }
