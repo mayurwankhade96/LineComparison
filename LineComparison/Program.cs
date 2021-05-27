40f1e4100f8ec98572ae5c5210e0f5c4e52068bb
Mayur Wankhade <mayur.wankhade2@gmail.com>
2021-05-27 14:06:20 +0530

    [Mayur] Refactor : variable names updated

--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -9,28 +9,28 @@ namespace LineComparison
             Console.WriteLine("Welcome to Line Comparison Computation Program");
             Line line = new Line();
             Line line1 = new Line();
-            double lineOne = line.LengthOfLine(5, 7, 10, 12);
-            double lineTwo = line1.LengthOfLine(4, 10, 6, 8);
+            double firstLine = line.LengthOfLine(5, 7, 10, 12);
+            double secondLine = line1.LengthOfLine(4, 10, 6, 8);
 
             //to check equality
-            if (lineOne.Equals(lineTwo))
+            if (firstLine.Equals(secondLine))
                 Console.WriteLine("lines are equal");
             else
                 Console.WriteLine("lines are not equal");
 
             //to compare
-            double compareValue = (lineOne.CompareTo(lineTwo));
+            double compareValue = (firstLine.CompareTo(secondLine));
             if (compareValue == 0)
             {
                 Console.WriteLine("lines are equal");
             }
             else if (compareValue == 1)
             {
-                Console.WriteLine("lineTwo is smaller than lineOne");
+                Console.WriteLine("Second line is smaller than first line");
             }
             else
             {
-                Console.WriteLine("lineOne is smaller than lineTwo");
+                Console.WriteLine("First line is smaller than second line");
             }
         }
     }
