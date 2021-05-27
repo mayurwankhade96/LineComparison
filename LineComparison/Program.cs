a82e2ad22bd633df120c0892475f8bbc5d323048
Mayur Wankhade <mayur.wankhade2@gmail.com>
2021-05-27 11:23:36 +0530

    [Mayur] Refactor : Equality feature added

--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -11,6 +11,12 @@ namespace LineComparison
             Line line1 = new Line();
             line.LengthOfLine(5, 7, 10, 12);
             line1.LengthOfLine(4, 10, 6, 8);
+
+            //to check equality
+            if (line.Equals(line1))
+                Console.WriteLine("lines are equal");
+            else
+                Console.WriteLine("lines are not equal");
         }
     }
 }
